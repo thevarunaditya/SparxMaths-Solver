12281b4c983afebe5f90614facef6338aa49c444
mono <letsgovaa@gmail.com>
2024-06-01 01:20:19 +0200

    Trying to fix setup.bat for easy playwright instlalation

--- setup.bat
+++ setup.bat
@@ -1,20 +1,12 @@
 @echo off
 
-REM Check if running as administrator
-NET SESSION >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    echo Running as administrator
-) ELSE (
-    echo Please run this script as an administrator
-    pause
-    exit
-)
+echo This will install chocolatey, npm, and dependencies for SparxSolver.
 
-@REM Installing Node
-winget install Schniz.fnm
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+choco feature enable -n=allowGlobalConfirmation
 
-fnm use --install-if-missing 20
+choco install nodejs
 
-@REM Installing Playwright
-npm install playwright
-npx playwright install
+npm install -g body-parser@^1.20.2 discord-webhook-node@^1.1.8 ejs@^3.1.10 express@^4.19.2 express-fileupload@^1.5.0 express-session@^1.18.0 fs-extra@^11.2.0 multer@^1.4.5-lts.1 playwright@^1.44.1 sharp@^0.33.4 util@^0.12.5
+
+pause
